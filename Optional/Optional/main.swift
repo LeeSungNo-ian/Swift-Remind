36f472771d315eb546b449a7e717210b05fb7cd3
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-11-23 22:52:05 +0900

    feat: Optional 변수 생성
    Optional 변수 생성

--- Optional/Optional/main.swift
+++ Optional/Optional/main.swift
@@ -1,13 +1,12 @@
 //
 //  main.swift
-//  Optional Practice
+//  Optional
 //
 //  Created by 이성노 on 2021/11/23.
 //
 
 import Foundation
 
-var number: Int? = 3
+var number: Int? = 5
 var name: String? = "이안"
-var high: Double? = 3.5
-
+var high: Double? = 3.3

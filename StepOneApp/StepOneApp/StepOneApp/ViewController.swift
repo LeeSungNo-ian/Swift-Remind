3b61773b324bcf1a7657dc1e21b1cfae9c344082
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-11-27 01:52:45 +0900

    feat: IBOutlet, IBAction 생성
    ViewController IBOutlet, IBAction 생성

--- StepOneApp/StepOneApp/StepOneApp/ViewController.swift
+++ StepOneApp/StepOneApp/StepOneApp/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
 
 }
 

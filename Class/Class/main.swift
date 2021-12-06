d3334bd260460c0545c511e33cf6e04867341164
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-12-06 20:04:40 +0900

    feat: class Book 생성
    class Book 생성

--- Class/Class/main.swift
+++ Class/Class/main.swift
@@ -17,23 +17,18 @@ class Dog {
     }
 }
 
-var bori = Dog()
-bori.name = "보리"
-print(bori.name)
-print(bori.weight)
-
-var choco = Dog()
-choco.name = "초코"
-print(choco.name)
-print(choco.weight)
-
 class Book {
-    var name = "책제목"
-    var price = 0
-    var page = 0
+    var name: String
+    var price: Int
+    var whereCom: String
+    var whoWrite: String
+    var page: Int
+    
+    init(name: String, price: Int, whereCom: String, whoWrite: String, page: Int) {
+        self.name = name
+        self.price = price
+        self.whereCom = whereCom
+        self.whoWrite = whoWrite
+        self.page = page
+    }
 }
-
-var book1 = Book()
-book1.name = "스위프트"
-book1.page = 200
-book1.price = 23000
